06932fd546cfff1822a5016863913498b2b90aae
Bhavy <bhavy@Bhavys-MacBook-Air.local>
2025-12-16 09:54:33 +0530

    config app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,11 +1,14 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-import streamlit as st
-from dotenv import load_dotenv
 
+# Load .env for local development
 load_dotenv()
 
+# Prompt
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "You are a helpful assistant."),
@@ -13,13 +16,17 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
+# LLM (works locally + Streamlit Cloud)
 llm = ChatGroq(
-    model="llama-3.1-8b-instant", 
-    temperature=0.5
+    model="llama-3.1-8b-instant",
+    temperature=0.5,
+    api_key=os.getenv("GROQ_API_KEY")
 )
 
+# Chain
 chain = prompt | llm | StrOutputParser()
 
+# UI
 st.title("🤖 Chatbot with Groq (LLaMA 3)")
 question = st.text_input("Ask something")
 
