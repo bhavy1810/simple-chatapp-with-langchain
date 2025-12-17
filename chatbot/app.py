4ee976e366b82c1e78607f72d70bc3ccf563442c
Bhavy <bhavy@Bhavys-MacBook-Air.local>
2025-12-17 09:50:18 +0530

    button update

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,14 +1,13 @@
 import os
+from urllib import response
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-# Load .env for local development
 load_dotenv()
 
-# Prompt
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "You are a helpful assistant."),
@@ -16,19 +15,18 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# LLM (works locally + Streamlit Cloud)
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
     temperature=0.5,
     api_key=os.getenv("GROQ_API_KEY")
 )
 
-# Chain
 chain = prompt | llm | StrOutputParser()
 
-# UI
 st.title("🤖 Chat with Promptly ")
 question = st.text_input("Ask something")
 
 if question:
-    st.write(chain.invoke({"question": question}))
+    response = chain.invoke({"question": question})
+    st.subheader("Answer")
+    st.code(response, language="markdown")
